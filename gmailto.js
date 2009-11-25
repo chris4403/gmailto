d39793b37226cc637c6c5a952c83c9b8c7070985
chris <chris4403@hatena.ne.jp>
2009-11-26 00:07:58 +0900

    first commit

---
+++ gmailto.js
@@ -0,0 +1,11 @@
+(function(){
+    var as = document.querySelectorAll('a');
+    for (var i = 0 , len = as.length ; i < len ; i ++){
+        var a = as[i];
+        if (a.href.indexOf('mailto:') == 0) {
+            var base = 'https://mail.google.com/mail/?view=cm&fs=1&tf=1%s';
+            a.href = base.replace('%s', a.href.replace('?','&').replace('mailto:','&to='));
+            a.target = '_blank';
+        }
+    }
+})();
